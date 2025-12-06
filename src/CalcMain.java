a07f342e81db094bf36316e509db69670795b877
Kerensky585 <37952431+Kerensky585@users.noreply.github.com>
2025-12-06 21:27:01 +0000

    Test project

---
+++ src/CalcMain.java
@@ -0,0 +1,26 @@
+
+
+
+public class CalcMain {
+
+    public static int NUMA;
+    public static int NUMB;
+
+    public static void main(String[] args) {
+        NUMA = 5;
+        NUMB = 5;
+        System.out.println("Hello, World!");
+        int result = addNumbers(NUMA, NUMB);
+        System.out.println("You added: " + NUMA + " with, " + NUMB + "\nTotal is: " + result);
+    }
+
+    /**
+     * addNumbers - adds two integer numbers together, return result.
+     * @param numa integer number
+     * @param numb integer number
+     * @return int total
+     */
+    public static int addNumbers(int numa, int numb){
+        return numa + numb;
+    }
+}
